669f7a53755d35ec7cd959a56c439f3facdfd9d6
DrMachoo <davitootooop@gmail.com>
2020-11-17 12:39:50 +0100

    Differential regning start formler UwU

--- Diffentialregning.py
+++ Diffentialregning.py
@@ -1,7 +1,36 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib.patches import Polygon
+#import numpy as np
+#import matplotlib.pyplot as plt
+#from matplotlib.patches import Polygon
 
-class differentialregning:
-    def __init__(self):
-        pass
+
+class diffenrentialregning:
+    def __init__(self, a, b, c):
+        self.a = a
+        self.b = b
+        self.c = c
+        #self.x = x
+        #self.DeltaX = DeltaX
+
+    def func(self, q):
+        return (self.a * q **2 + self.b * q + self.c)
+
+    def Differential(self, x, DeltaX):
+        n = 0
+
+
+        while True:
+            a = (self.func(x + DeltaX) - self.func(x)) / ((x + DeltaX) - x)
+            # print(DeltaX)
+            print(a)
+            DeltaX = DeltaX / 2
+            n += 1
+            if n == 40:
+                break
+
+        print (a)
+        return a
+
+
+
+objekt1 = diffenrentialregning(6, 4, 2)
+objekt1.Differential(4,7)
