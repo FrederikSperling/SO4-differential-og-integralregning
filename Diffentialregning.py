3a63cf501bb150cdb437291e44fe32a05c9808d7
casper1357 <casper@hhcc.dk>
2020-11-17 11:30:47 +0100

    Startup med klasser

---
+++ Diffentialregning.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.patches import Polygon
+
+class differentialregning:
+    def __init__(self):
+        pass
---
+++ integralregning.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.patches import Polygon
+
+class integralregning:
+    def __init__(self):
+        pass
