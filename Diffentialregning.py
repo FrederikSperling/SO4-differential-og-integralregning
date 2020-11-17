76e0a568afbfc7d814303e47bcb454ce4c6ffbdc
DrMachoo <davitootooop@gmail.com>
2020-11-17 12:56:13 +0100

    Differential regning start formler UwU
    Patch 1.01
    - Lavede lidt dokumentation

--- Diffentialregning.py
+++ Diffentialregning.py
@@ -8,23 +8,27 @@ class diffenrentialregning:
         self.c = c
 
 
+    # Metode der styrer hvilken formel vi skal bruge.
+    # Lige nu har vi kun formel der hedder "ax^2 + bx + c"
     def func(self, q):
         return (self.a * q **2 + self.b * q + self.c)
 
     def Differential(self, x, DeltaX):
         n = 0
 
-
+        # Vi bruger linjens ligning mellem 2 punkter for at finde hældningen på den linje der går igennem x og x + DeltaX
         while True:
             a = (self.func(x + DeltaX) - self.func(x)) / ((x + DeltaX) - x)
 
-            print(a)
+            # Ved at dividere DeltaX med 2 hele tiden får vi den til at gå mod 0
+            # Dette betyder at vi kommer tættere på at have den tangent der går igennem x
+
+            #print(a)
             DeltaX = DeltaX / 2
             n += 1
             if n == 40:
                 break
 
-        print (a)
         return a
 
 
