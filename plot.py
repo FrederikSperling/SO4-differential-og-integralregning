d8a6f2dc26dcc527fa5e62ac1029bff4fc29924c
casp028a <casp028a@edu.aarhustech.dk>
2020-11-18 22:40:45 +0100

    Har tilføjet et par ting til vores graf

--- plot.py
+++ plot.py
@@ -4,9 +4,14 @@ from matplotlib.patches import Polygon
 from Integralregning import *
 from Diffentialregning import *
 
+#Desto mere præcis svar man gerne have, jo højere antal søjler (streg) skriver man bare ind i klassen hvor mange man vil have.
+#Hvis man gerne vil have et meget præcist svar kommer det til at vente lidt fordi det tager en del tid at beregne for computeren.
+#F.eks. 1000 søjler = 1 sekund, 100.000.000 = 20 min, 1.000.000 = 10 sekunder
+#Man kan også fjerne print for x, y og streg hvis det skal gå hurtigere.
+
 Integral = Integralregning(-10, 10, 10000, 2, 6, 5, 2)
 
-class Testplot:
+class graf:
     def __init__(self):
         Integral.ILommeregner()
         #Minimum- og maximum x-værdi på grafen. Ud fra det laver den korresponderende y-akse.
@@ -22,20 +27,32 @@ class Testplot:
             else:
                 self.x = np.linspace(0, Integral.b * Integral.b * 0.5)
 
-        self.y = self.func(self.x)
+        self.y = Integral.func(self.x)
         fig, ax = plt.subplots()
         ax.plot(self.x, self.y, 'r', linewidth=2)
         ax.set_ylim(bottom=0)
-        ax.set_xticks((Integral.a, Integral.b))
-        ax.vlines(x=[Integral.a, Integral.b], ymin=0, ymax=[self.func(Integral.a), self.func(Integral.b)])
-        ax.set_xticklabels(('$a: $' + str(Integral.a), '$b: $' + str(Integral.b)))
+        #ax.set_xticks((Integral.a, Integral.b))
+        ax.vlines(x=[Integral.a, Integral.b], ymin=0, ymax=[Integral.func(Integral.a), Integral.func(Integral.b)], colors='r')
+        #ax.set_xticklabels(('$a: $' + str(Integral.a), '$b: $' + str(Integral.b)))
         ax.text(0.5 * (Integral.a + Integral.b), 100, r"$\int_a^b f(x)\mathrm{d}x$", horizontalalignment='center', fontsize=20)
         ax.text(0.5 * (Integral.a + Integral.b), 200, r"Areal er: "+str(Integral.sumAreal), horizontalalignment='center',
                 fontsize=10)
-        plt.show()
+        plt.xlabel('hej2')
+        plt.ylabel('hej')
 
-    def func(self, x):
-        return (Integral.A * x ** Integral.potens + Integral.B * x + Integral.C)
+        ax.xaxis.label.set_color('r')
+        ax.yaxis.label.set_color('r')
+
+        plt.fill_between(self.x, self.y, -10, color='r', alpha=0.2)
+        ax.grid(True, color='grey', linestyle='-', linewidth=1)
+
+        #n = 0
+        #while True:
+            #n += 1
+            #plt.axvspan(Integral.a, Integral.a + Integral.DeltaX, color='red', alpha=0.5)
+            #if n == 100:
+                #break
+        plt.show()
 
 
 
