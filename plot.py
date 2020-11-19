2fef06c5c9b9279c5459b9ee1b7f6d9a2852d036
casper1357 <casper@hhcc.dk>
2020-11-19 12:39:21 +0100

    Gjordt så den filler rigtigt pog

--- plot.py
+++ plot.py
@@ -9,7 +9,7 @@ from Diffentialregning import *
 #F.eks. 1000 søjler = 1 sekund, 100.000.000 = 20 min, 1.000.000 = 10 sekunder
 #Man kan også fjerne print for x, y og streg hvis det skal gå hurtigere.
 
-Integral = Integralregning(1, 10, 100, 2, 6, 5, 2)
+Integral = Integralregning(-100, 10, 100, 1, 6, 5, 2)
 Integral.ILommeregner()
 class graf:
     def __init__(self):
@@ -18,19 +18,21 @@ class graf:
         self.x = np.linspace(Integral.a - Integral.b * 0.5, Integral.b * 1.5)
         self.y = Integral.func(self.x)
         fig, ax = plt.subplots()
-        ax.plot(self.x, self.y, 'r', linewidth=2)
+        ax.plot(self.x, self.y, 'black', linewidth=2)
         ax.set_ylim(bottom=0)
-        #ax.set_xticks((Integral.a, Integral.b))
-        ax.vlines(x=[Integral.a, Integral.b], ymin=0, ymax=[Integral.func(Integral.a), Integral.func(Integral.b)], colors='r')
+        #ax.set_xticks((Integral.a, Integral.b)
+        ax.vlines(x=[Integral.a, Integral.b], ymin=0, ymax=[Integral.func(Integral.a), Integral.func(Integral.b)], colors='blue')
         #ax.set_xticklabels(('$a: $' + str(Integral.a), '$b: $' + str(Integral.b)))
         ax.text(0.5 * (Integral.a + Integral.b), 100, r"$\int_a^b f(x)\mathrm{d}x$", horizontalalignment='center', fontsize=20)
-        ax.text(0.5 * (Integral.a + Integral.b), 200, r"Areal er: "+str(Integral.sumAreal), horizontalalignment='center',
+        ax.text(0.5 * (Integral.a + Integral.b), 100, r"Areal er: "+str(Integral.sumAreal), horizontalalignment='center',
                 fontsize=10)
-        plt.xlabel('hej2')
-        plt.ylabel('hej')
-        ax.xaxis.label.set_color('r')
-        ax.yaxis.label.set_color('r')
-        plt.fill_between(self.x, self.y, -10, color='r', alpha=0.2)
+        plt.xlabel('x')
+        plt.ylabel('y')
+        ax.xaxis.label.set_color('black')
+        ax.yaxis.label.set_color('black')
+        fillx = np.linspace(Integral.a, Integral.b)
+        filly = Integral.func(fillx)
+        plt.fill_between(fillx, filly, 0, color='blue', alpha=0.2)
         ax.grid(True, color='grey', linestyle='-', linewidth=1)
         plt.show()
 
